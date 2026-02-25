d6d24eb03f7178f2b9167e75cbdd500ee9d08aba
Jeremy Soller <jackpot51@gmail.com>
2026-02-25 10:35:12 -0700

    Drop upstreamed patches and update dependencies

--- src/shell/element/stack/tab_text.rs
+++ src/shell/element/stack/tab_text.rs
@@ -85,6 +85,7 @@ impl TabText {
             shaping: Shaping::Advanced,
             line_height: LineHeight::default(),
             wrapping: cosmic::iced::advanced::text::Wrapping::None,
+            ellipsize: cosmic::iced::advanced::text::Ellipsize::None,
         })
     }
 }
